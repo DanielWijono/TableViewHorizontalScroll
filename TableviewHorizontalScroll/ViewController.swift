a2d95c5f1eeb5206c90891fb15fe3916376a019e
daniel wijono <danielwijono999@gmail.com>
2020-05-27 21:38:02 +0700

    Initial Commit

---
+++ TableviewHorizontalScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableviewHorizontalScroll
+//
+//  Created by Daniel Wijono on 27/05/20.
+//  Copyright © 2020 Daniel Wijono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
